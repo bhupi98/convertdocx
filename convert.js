d7a05660dc071fc83d08bff323c427878f5ac672
bhupender <bhupender.cool98@gmail.com>
2023-09-13 10:33:55 +0530

    initial commit

---
+++ convert.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const path = require("path");
+const libre = require("libreoffice-convert");
+libre.convertAsync = require("util").promisify(libre.convert);
+
+async function main(buffer) {
+  const ext = ".pdf";
+  // const inputPath = path.join(__dirname, "/resources/example.docx");
+  const outputPath = path.join(__dirname, `/resources/example${ext}`);
+
+  // Read file
+  //const docxBuf = await fs.readFile(inputPath);
+
+  // Convert it to pdf format with undefined filter (see Libreoffice docs about filter)
+  let pdfBuf = await libre.convertAsync(buffer, ext, undefined);
+  console.log("pdfBuf", pdfBuf);
+  // Here in done you have pdf file which you can save or transfer in another stream
+  return pdfBuf;
+}
+
+module.exports = { main: main };
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const multer = require("multer");
+const PORT = 9000;
+
+const { main } = require("./convert");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const storage = multer.memoryStorage(); // Store the uploaded file in memory as a buffer
+const upload = multer({ storage: storage });
+app.post("/upload", upload.single("file"), function (req, res) {
+  console.log("file", req.file);
+  main(req.file.buffer)
+    .then(function (data) {
+      res.status(200).send(data);
+    })
+    .catch(function (err) {
+      res.status(500).send(err);
+    });
+});
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
