51dd86b20456656985edc8b3711f96f19132dbc8
bhupender <bhupender.cool98@gmail.com>
2023-09-14 10:02:06 +0530

    added limit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const multer = require("multer");
+const axios = require("axios");
 const PORT = 8080;
 
 const { main } = require("./convert");
 const app = express();
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 // app.use(
 //   express.raw({
 //     type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
 //   })
 // );
-app.use(express.urlencoded({ extended: true }));
+
 const storage = multer.memoryStorage(); // Store the uploaded file in memory as a buffer
 const upload = multer({ storage: storage });
 app.post("/docxtopdf", async (req, res) => {
@@ -35,8 +37,11 @@ app.post("/upload", upload.single("file"), function (req, res) {
       res.status(500).send(err);
     });
 });
-app.get("/start", (req, res) => {
-  console.log("im being called n times  ......");
-  res.status(200).send("Application is running");
-});
+// app.post("/start", async (req, res) => {
+//   let base64 = req.body.base64;
+//   let buffer = Buffer.from(base64, "base64").toString("utf-8");
+//   let data = await axios.post("http://34.228.230.17:8080/docxtopdf", buffer);
+//   console.log("data", data.data);
+//   res.status(200).send("Application is running");
+// });
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
